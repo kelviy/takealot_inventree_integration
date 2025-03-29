578de60010fca34396ba75835a1a1ba362a06ca1
kelviy <52874397+kelviy@users.noreply.github.com>
2025-03-29 13:59:43 +0200

    Add script and other folders

---
+++ cp_statics.sh
@@ -0,0 +1,3 @@
+# to be run from plugin folder (leadtime_order_sync) from plugins folder for docker installation
+mkdir -p ../../static/takealot_integration/js
+cp ./static/takealot_integration/js/TakealotPlugin.js ../../static/takealot_integration/js/
