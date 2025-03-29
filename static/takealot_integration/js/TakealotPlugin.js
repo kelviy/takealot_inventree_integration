af01f657289e853f3f224be7cff0ec44cfe3bcc3
kelviy <52874397+kelviy@users.noreply.github.com>
2025-03-29 14:34:00 +0200

    javascript fix

--- static/takealot_integration/js/TakealotPlugin.js
+++ static/takealot_integration/js/TakealotPlugin.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   container.innerHTML = '<div id="loader">Loading...</div>';
 
   // Fetch the data from the endpoint
-  fetch('/plugins/takealot/fetch-takealot-data/')
+  fetch('/plugins/takealotintegrator/fetch-takealot-data/')
     .then(function(response) {
       return response.json();
     })
